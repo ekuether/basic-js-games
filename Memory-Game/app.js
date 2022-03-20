80c5d53da32eb7273538fda1bc1d3ff64f6e3ae3
Ethen Kuether <ekuether88@gmail.com>
2022-03-20 17:47:51 -0500

    Initial Commit
    Includes two games: rock, paper, scissors and Memory game. Also added a README

---
+++ Memory-Game/app.js
@@ -0,0 +1,117 @@
+const cardArray = [
+
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    }
+
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
+
+function createBoard () {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        gridDisplay.appendChild(card)
+    }
+}
+createBoard()
+
+function checkMatch() {
+    const cards = document.querySelectorAll('img')
+    const option1Id = cardsChosenIds[0]
+    const option2Id = cardsChosenIds[1]
+    
+    if (option1Id == option2Id) {
+        alert('You have clicked the same image')
+        cards[option1Id].setAttribute('src', 'images/blank.png')
+        cards[option2Id].setAttribute('src', 'images/blank.png')
+    }
+
+    else if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match!')
+        cards[option1Id].setAttribute('src', 'images/white.png')
+        cards[option2Id].setAttribute('src', 'images/white.png')
+        cards[option1Id].removeEventListener('click', flipCard)
+        cards[option2Id].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    }
+
+    else {
+        cards[option1Id].setAttribute('src', 'images/blank.png')
+        cards[option2Id].setAttribute('src', 'images/blank.png')
+        alert('Sorry, try again!')
+
+    }
+    resultDisplay.textContent = cardsWon.length
+    cardsChosen = []
+    cardsChosenIds = []
+
+    if (cardsWon.length == (cardArray.length/2)) {
+        resultDisplay.textContent = 'Congratulations you found them all!'
+    }
+
+}
+
+function flipCard() {
+   const cardId = this.getAttribute('data-id')
+   cardsChosen.push(cardArray[cardId].name)
+   cardsChosenIds.push(cardId)
+   this.setAttribute('src', cardArray[cardId].img)
+   if (cardsChosen.length === 2) {
+       setTimeout(checkMatch, 500)
+   }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Basic-js-games
+
+This repo is to store basic games I made (using instructions from freecodecamp.org on youtube)
+
+## Rock, Paper, Scissors
+
+Game of rock, paper, scissors. 
+Very basic, but will be updated.
+
+## Memory Matching
+
+Game to match the 12 cards (6 different icons)
+Will be updated later as well
