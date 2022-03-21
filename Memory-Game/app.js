163430e7620a7a1d57b414a796d0473238f6502f
Ethen Kuether <ekuether88@gmail.com>
2022-03-20 22:19:13 -0500

    Added an 'else' to function 'flipcard' to prevent user clicking on the same card to reduce logic complexity in function 'checkMatch'. Also add .pop to arrays to prevent redeclaring them

--- Memory-Game/app.js
+++ Memory-Game/app.js
@@ -1,29 +1,4 @@
-const cardArray = [
-
-    {
-        name: 'cheeseburger',
-        img: 'images/cheeseburger.png'
-    },
-    {
-        name: 'fries',
-        img: 'images/fries.png'
-    },
-    {
-        name: 'hotdog',
-        img: 'images/hotdog.png'
-    },
-    {
-        name: 'ice-cream',
-        img: 'images/ice-cream.png'
-    },
-    {
-        name: 'milkshake',
-        img: 'images/milkshake.png'
-    },
-    {
-        name: 'pizza',
-        img: 'images/pizza.png'
-    },
+const availableImages = [
     {
         name: 'cheeseburger',
         img: 'images/cheeseburger.png'
@@ -48,15 +23,15 @@ const cardArray = [
         name: 'pizza',
         img: 'images/pizza.png'
     }
-
 ]
 
+const cardArray = availableImages.concat(availableImages)
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
 const resultDisplay = document.querySelector('#result')
-let cardsChosen = []
-let cardsChosenIds = []
+const cardsChosen = []
+const cardsChosenIds = []
 const cardsWon = []
 
 function createBoard () {
@@ -75,13 +50,7 @@ function checkMatch() {
     const option1Id = cardsChosenIds[0]
     const option2Id = cardsChosenIds[1]
     
-    if (option1Id == option2Id) {
-        alert('You have clicked the same image')
-        cards[option1Id].setAttribute('src', 'images/blank.png')
-        cards[option2Id].setAttribute('src', 'images/blank.png')
-    }
-
-    else if (cardsChosen[0] === cardsChosen[1]) {
+    if (cardsChosen[0] === cardsChosen[1]) {
         alert('You found a match!')
         cards[option1Id].setAttribute('src', 'images/white.png')
         cards[option2Id].setAttribute('src', 'images/white.png')
@@ -93,12 +62,15 @@ function checkMatch() {
     else {
         cards[option1Id].setAttribute('src', 'images/blank.png')
         cards[option2Id].setAttribute('src', 'images/blank.png')
+        cards[option1Id].addEventListener('click', flipCard)
         alert('Sorry, try again!')
 
     }
     resultDisplay.textContent = cardsWon.length
-    cardsChosen = []
-    cardsChosenIds = []
+    cardsChosen.pop()
+    cardsChosen.pop()
+    cardsChosenIds.pop()
+    cardsChosenIds.pop()
 
     if (cardsWon.length == (cardArray.length/2)) {
         resultDisplay.textContent = 'Congratulations you found them all!'
@@ -113,5 +85,8 @@ function flipCard() {
    this.setAttribute('src', cardArray[cardId].img)
    if (cardsChosen.length === 2) {
        setTimeout(checkMatch, 500)
+   }
+   else {
+       this.removeEventListener('click', flipCard);
    }
 }
